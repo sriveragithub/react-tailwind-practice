9ba4c8e250fd000ded855f77b1654d03496b445e
Skyler Rivera <skylerrivera17@yahoo.com>
2021-05-09 14:23:25 -0400

    testing using Tailwind CSS to style a hero page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [isAnimated, setIsAnimated] = useState(false)
+  useEffect(() => {
+    setIsAnimated(true)
+  }, [])
+
   return (
-    <div>
-      <h1>Hello world</h1>
+    <div
+      className="min-h-screen w-screen flex justify-center items-center flex-col-reverse lg:flex-row lg:justify-around"
+      style={{
+        background: 'linear-gradient(to bottom, #F0F4FD, #A1A384'
+    }}>
+      <div className={`${isAnimated ? 'scale-y-100' : 'scale-y-0'} mt-10 text-center lg:text-left transform transition duration-1000 ease-in-out`}>
+        <h1 className='font-black text-5xl lg:text-6xl' >Fresh Healthy<br />
+        <span className='font-quicksand font-light'>Delicious Salads</span></h1>
+        <p className='text-base text-gray-700'>We make fresh and healthy foods</p>
+        <span className='inline-block bg-indigo-500 px-10 py-5 rounded-full shadow-lg uppercase text-lg tracking-wider mt-5'>Explore</span>
+      </div>
+      <img src={process.env.PUBLIC_URL + '/assets/food.svg'} alt='food' className={`${isAnimated ? 'scale-150' : 'translate-x-full translate-y-full'} w-screen lg:max-w-xl transform transition duration-1000 ease-in-out`} />
     </div>
   );
 }
